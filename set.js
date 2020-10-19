b4d12938d2b7f5aba5fa245f09e0eebfec0b46a4
jinchuu1391 <62422486+jinchuu1391@users.noreply.github.com>
2020-10-19 10:24:49 +0900

    주석 추가

--- set.js
+++ set.js
@@ -3,10 +3,12 @@ class Set {
     this.items = {};
   }
   has(value) {
+    // 어떤 원소가 set(집합)에 포함되어 있는지 여부를 불리언 값으로 반환한다
     return this.items.hasOwnProperty(value);
     // hasOwnProperty -> 객체가 특정 프로퍼티를 가지고 있는지를 나타내는 불리언 값을 반환한다
   }
   add(value) {
+    // 원소를 추가한다
     if (!this.has(value)) {
       this.items[value] = value; // 키 - 값 쌍을 동일하게 저장해야 나중에 값을 찾기 편하다
       return true;
@@ -14,6 +16,7 @@ class Set {
     return false;
   }
   remove(value) {
+    // 원소를 삭제한다
     if (this.has(value)) {
       delete this.items[value];
       return true;
@@ -21,15 +24,18 @@ class Set {
     return false;
   }
   clear() {
+    // 모든 원소를 삭제한다
     this.items = {};
   }
   size() {
+    // 원소 개수를 반환한다
     return Object.keys(this.items).length;
   }
   values() {
     return Object.keys(this.items);
   }
   union(otherSet) {
+    // 합집합 -> 두 집합 중 어느 한쪽이라도 포함된 원소로 구성된 집합을 구한다
     let unionSet = new Set(); // 합집합을 담을 변수 생성
     let values = this.values();
     for (let i = 0; i < values.length; i++) {
@@ -44,6 +50,7 @@ class Set {
     return unionSet;
   }
   intersection(otherSet) {
+    // 교집합 -> 두 집합 모두 포함되어 있는 원소로 구성된 집합을 구한다
     let intersectionSet = new Set(); // 교집합을 담을 변수 생성
     let values = this.values();
     for (let i = 0; i < values.length; i++) {
@@ -56,6 +63,7 @@ class Set {
     return intersectionSet;
   }
   difference(otherSet) {
+    // 차집합 -> 첫 번째 집합에는 있지만 두 번째 집합에는 없는 원소로 구성된 집합을 구한다
     let differenceSet = new Set(); // 차집합을 담을 변수 생성
     let values = this.values();
     for (let i = 0; i < values.length; i++) {
@@ -68,6 +76,7 @@ class Set {
     return differenceSet;
   }
   subSet(otherSet) {
+    // 부분집합 -> 어떤 집합이 다른 집합의 일부인지 확인한다
     if (this.size() > otherSet.size()) {
       return false;
     } else {
