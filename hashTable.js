e3f96ebfce003b91d59cb1f57d90975e81b452b1
jinchuu1391 <62422486+jinchuu1391@users.noreply.github.com>
2020-10-22 13:51:18 +0900

    기본 해쉬테이블

---
+++ hashTable.js
@@ -0,0 +1,23 @@
+class HashTable {
+  constructor() {
+    this.table = [];
+  }
+  loseloseHashCode(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % 37;
+  }
+  put(key, value) {
+    let position = this.loseloseHashCode(key);
+    console.log(position + " - " + value);
+    this.table[position] = value;
+  }
+  get(key) {
+    return this.table[this.loseloseHashCode(key)];
+  }
+  remove(key) {
+    this.table[this.loseloseHashCode(key)] = undefined;
+  }
+}
